756e4994de482546728f126a89602a5c17b4542f
Cha0s aka DonkeyJJLove or… <donkeyjjlove@protonmail.com>
2026-08-18 16:15:26 +0200

    feat(cyber-lion): expose identity contract

---
+++ cyber_lion/contracts/__init__.py
@@ -0,0 +1,3 @@
+"""Versioned Cyber-Lion cross-repository contracts."""
+from .identity import EntityIdentity, IdentityValidationError, aid_from_entity, entity_from_aid
+__all__ = ["EntityIdentity", "IdentityValidationError", "aid_from_entity", "entity_from_aid"]
