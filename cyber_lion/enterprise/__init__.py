d0f1c8587be40fcef9f8df3409fc5ba063ede998
Cha0s aka DonkeyJJLove or… <donkeyjjlove@protonmail.com>
2026-08-23 12:54:53 +0200

    E001 F008: export canonical PDP

--- cyber_lion/enterprise/__init__.py
+++ cyber_lion/enterprise/__init__.py
@@ -6,9 +6,10 @@ from .event_graph_projection import agent_node_from_registry_key,event_to_graph_
 from .federation import RepositoryFederationRegistry,RepositoryManifest
 from .models import AgentSpec,EnterpriseModelError,MissionSpec,MosaicDelta,SwarmSpec
 from .planner import SwarmPlanner
+from .policy_gate import CanonicalPolicyDecisionPoint,PDPResult,PolicyGateError,authority_contains
 from .swarm_governor import SwarmGovernorLeaseStore,SwarmGovernorStateError
 from .swarm_role_allocator import SwarmRoleAllocator
 from .swarm_formation_manager import SwarmFormationManager
 from .swarm_status import SwarmStatusStore,SwarmStatusStateError
 from .swarm_status_projection import validate_status_projection,classify_live_master
-__all__=["ActionProposal","AgentRegistryStateError","AgentRegistryStore","AgentSpec","EnterpriseGraphStateError","EnterpriseGraphStore","EnterpriseModelError","ExecutionControlPlane","ExecutionReceipt","GateDecision","MissionSpec","MosaicDelta","RepositoryFederationRegistry","RepositoryManifest","SwarmPlanner","SwarmSpec","SwarmGovernorLeaseStore","SwarmGovernorStateError","SwarmRoleAllocator","SwarmFormationManager","SwarmStatusStore","SwarmStatusStateError","validate_status_projection","classify_live_master","agent_node_from_registry_key","event_to_graph_records"]
+__all__=["ActionProposal","AgentRegistryStateError","AgentRegistryStore","AgentSpec","EnterpriseGraphStateError","EnterpriseGraphStore","EnterpriseModelError","ExecutionControlPlane","ExecutionReceipt","GateDecision","MissionSpec","MosaicDelta","RepositoryFederationRegistry","RepositoryManifest","SwarmPlanner","SwarmSpec","SwarmGovernorLeaseStore","SwarmGovernorStateError","SwarmRoleAllocator","SwarmFormationManager","SwarmStatusStore","SwarmStatusStateError","validate_status_projection","classify_live_master","agent_node_from_registry_key","event_to_graph_records","CanonicalPolicyDecisionPoint","PDPResult","PolicyGateError","authority_contains"]
