cf5bf8e2dfa82fb8218c1ab5318bd960d73c98e6
Cha0s aka DonkeyJJLove or… <donkeyjjlove@protonmail.com>
2026-09-07 22:23:08 +0200

    test: rebind current production scan digest for R22E

--- tools/p0_surface_closure_campaign.py
+++ tools/p0_surface_closure_campaign.py
@@ -5,7 +5,7 @@ from typing import Mapping,Tuple
 from cyber_lion.contracts.complete_mediation import EffectSurfaceInventory
 from tools.p0_surface_closure_campaign_contract import ProviderFamilyClosurePlan,SurfaceClosureCampaign,SurfaceClosureWorkItem
 
-EXPECTED_SCAN_DIGEST="3c1810087e8da8e9a5646de8aaef075361e4a0f35060a891336900d9df51dc48"
+EXPECTED_SCAN_DIGEST="11a80e24a5713d222e59abadc9d8ff618808a3a6930b26b06c39d1df48466fba"
 CERTIFIED_PARTIAL_SURFACE="8c6d0020a0816d674a783504d2a8ccc25e3e75c0d446057ba3f4450bd768f687"
 MOON_PROVIDERS=frozenset({
     ".github.workflows.moon-file-write.yml",
