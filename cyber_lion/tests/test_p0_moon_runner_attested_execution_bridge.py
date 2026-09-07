7d1a3fcc03b900aa4fd84c696d9c17660fdce1cd
Cha0s aka DonkeyJJLove or… <donkeyjjlove@protonmail.com>
2026-09-08 01:28:39 +0200

    test(r22i): bind runner candidate assertion to current scan

--- cyber_lion/tests/test_p0_moon_runner_attested_execution_bridge.py
+++ cyber_lion/tests/test_p0_moon_runner_attested_execution_bridge.py
@@ -27,7 +27,7 @@ from tools.p0_moon_runner_attested_bridge_contract import (
 import tools.p0_moon_runner_attested_execution_bridge as bridge
 
 REPOSITORY = "DonkeyJJLove/ai_platform"
-CURRENT_SCAN = "a45c1fc2be1846d4db49dc3c348f59e576f062358741b6c68da3836adb923d14"
+CURRENT_SCAN = "c643ab174bec81dc86fde535be72230c88cfc557a2ca5596f9362db259d02724"
 HISTORICAL_SCAN = "2e509f22b7684e465dbebba73886aa9eae74f166480cb7e46d5be90a02a566d3"
 LIVE_SOURCE_REVISION = "830f8c2e5561655dc35118c97f4574acc3bf0816"
 WORKFLOW_SOURCE = "tools/p0_moon_runner_attested_execution_bridge.workflow.source.yml"
