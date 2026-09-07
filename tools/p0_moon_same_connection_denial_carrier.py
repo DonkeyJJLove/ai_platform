c97d39f918a001a3f02622495c5343ec680b3a54
Cha0s aka DonkeyJJLove or… <donkeyjjlove@protonmail.com>
2026-09-07 23:55:45 +0200

    test(r22g): rebind same-connection scan pin

--- tools/p0_moon_same_connection_denial_carrier.py
+++ tools/p0_moon_same_connection_denial_carrier.py
@@ -18,7 +18,7 @@ from tools.p0_moon_seven_binding import FENCE,PERMISSION
 from tools.p0_moon_attack_registry import live_attacks
 from tools.p0_readonly_sqlite_boundary import open_readonly_sqlite
 
-EXPECTED_SCAN_DIGEST="11a80e24a5713d222e59abadc9d8ff618808a3a6930b26b06c39d1df48466fba"
+EXPECTED_SCAN_DIGEST="a45c1fc2be1846d4db49dc3c348f59e576f062358741b6c68da3836adb923d14"
 FENCE_PATH="/home/d2j3/.lion-moon-file-write-fence.sqlite3"
 TARGET_PATH="/home/d2j3/lion-p0-moon-replace-live-cert-r1.canary"
 EFFECT_KEY="3ad38b9be4ea737d77c672f97f430cb32a4ba432327f5d1086e38f18327dc4c8"
