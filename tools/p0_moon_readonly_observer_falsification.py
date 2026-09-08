1d6302a3a0ebe25dc17cc14ac20afd4d8285384f
Cha0s aka DonkeyJJLove or… <donkeyjjlove@protonmail.com>
2026-09-08 22:53:58 +0200

    R23: rebind readonly observer tool scan currentness to combined frontier

--- tools/p0_moon_readonly_observer_falsification.py
+++ tools/p0_moon_readonly_observer_falsification.py
@@ -13,7 +13,7 @@ from tools.p0_moon_seven_binding import DIRECT_OBSERVER_BLOCKED,FENCE,PERMISSION
 from tools.p0_moon_attack_registry import live_attacks
 from tools.p0_readonly_sqlite_boundary import open_readonly_sqlite
 
-EXPECTED_SCAN_DIGEST="29cc58638fcac156590e296b893f5aa11fb8791770ba277c77fe8adc1eb8cec4"
+EXPECTED_SCAN_DIGEST="5f6561edcd368c2acce5f0e216bc9e21324da2913035e92ef175b7aca5b773a2"
 FENCE_PATH="/home/d2j3/.lion-moon-file-write-fence.sqlite3"
 TARGET_PATH="/home/d2j3/lion-p0-moon-replace-live-cert-r1.canary"
 TABLE="moon_file_write_effect"
