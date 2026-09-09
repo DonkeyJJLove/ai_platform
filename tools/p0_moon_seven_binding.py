303c5787b201a69074976f643bf7b89b6aa0c7a7
Cha0s aka DonkeyJJLove or… <donkeyjjlove@protonmail.com>
2026-09-10 01:57:25 +0200

    LPCL: rebind seven-surface binding to final production scan

--- tools/p0_moon_seven_binding.py
+++ tools/p0_moon_seven_binding.py
@@ -13,7 +13,7 @@ from tools.p0_moon_seven_binding_contract import (
     MoonSafeAttackSpec,MoonSevenBindingPlan,MoonSurfaceEvidenceBundle,
 )
 
-EXPECTED_SCAN_DIGEST="9c93b7bc51ae9329c86fa59fae486eb35281f5716c1c7c267e427fbc91764d1a"
+EXPECTED_SCAN_DIGEST="36eff7437ccca98f1b473a7757e425d5613fb7a60e81c45b63cf2a4a82738048"
 SEVEN=tuple(sorted(RECEIPT_IMPLIED_MOON_SURFACES))
 PERMISSION="dbff98ee0801784d8616fc32d67dfbb2ea19fbfcc1cfbda829cf904953f5631b"
 FENCE=set(SEVEN)-{PERMISSION}
