a5f30682997af9b5d4229543b3637641dae0b3df
Cha0s aka DonkeyJJLove or… <donkeyjjlove@protonmail.com>
2026-09-10 01:58:21 +0200

    LPCL: rebind runner bridge test to final production scan

--- cyber_lion/tests/test_p0_moon_runner_attested_execution_bridge.py
+++ cyber_lion/tests/test_p0_moon_runner_attested_execution_bridge.py
@@ -27,7 +27,7 @@ from tools.p0_moon_runner_attested_bridge_contract import (
 import tools.p0_moon_runner_attested_execution_bridge as bridge
 
 REPOSITORY = "DonkeyJJLove/ai_platform"
-CURRENT_SCAN = "593ec6d0779b4a8e041619f1fa45f4ddbda0aa2f1aec068f145b8ea70efbd767"
+CURRENT_SCAN = "36eff7437ccca98f1b473a7757e425d5613fb7a60e81c45b63cf2a4a82738048"
 HISTORICAL_SCAN = "2e509f22b7684e465dbebba73886aa9eae74f166480cb7e46d5be90a02a566d3"
 LIVE_SOURCE_REVISION = "830f8c2e5561655dc35118c97f4574acc3bf0816"
 WORKFLOW_SOURCE = "tools/p0_moon_runner_attested_execution_bridge.workflow.source.yml"
