34747d7a4deb519bfd1ad98ef5977b597ebefded
Cha0s aka DonkeyJJLove or… <donkeyjjlove@protonmail.com>
2026-09-10 00:22:19 +0200

    LPCL: export canonical RUN and fleet mission contracts

--- cyber_lion/process_language/__init__.py
+++ cyber_lion/process_language/__init__.py
@@ -1,11 +1,31 @@
-"""LPCL surface language and historical RUN translation helpers."""
+"""LPCL surfaces, canonical process compilation and historical RUN helpers."""
 from .lpcl import LPCLParseError, parse_lpcl, render_lpcl
 from .legacy_run import LegacyRunAdapter, LegacyRunResult
+from .fleet_mission import FleetMissionContractError, FleetMissionIR, FleetRoleSpec
+from .canonical_run import (
+    CANONICAL_SURFACE_VERSION,
+    CanonicalRunAST,
+    CanonicalRunCompilation,
+    CanonicalRunError,
+    CanonicalRunPhase,
+    compile_canonical_run,
+    parse_canonical_run,
+)
 
 __all__ = [
+    "CANONICAL_SURFACE_VERSION",
+    "CanonicalRunAST",
+    "CanonicalRunCompilation",
+    "CanonicalRunError",
+    "CanonicalRunPhase",
+    "FleetMissionContractError",
+    "FleetMissionIR",
+    "FleetRoleSpec",
     "LPCLParseError",
     "LegacyRunAdapter",
     "LegacyRunResult",
+    "compile_canonical_run",
+    "parse_canonical_run",
     "parse_lpcl",
     "render_lpcl",
 ]
