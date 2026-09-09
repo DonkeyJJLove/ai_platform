9bb47730bfb2d5d76240cd96e604f0d731bb6956
Cha0s aka DonkeyJJLove or… <donkeyjjlove@protonmail.com>
2026-09-10 01:56:21 +0200

    VKT-R3: raise kubelet pod capacity and use absolute drone shell

--- tools/lion_k3s_pod_materializer.py
+++ tools/lion_k3s_pod_materializer.py
@@ -192,7 +192,7 @@ def manifest() -> dict:
                         "terminationGracePeriodSeconds": 5,
                         "containers": [{
                             "name": "drone", "image": "python:3.12-alpine", "imagePullPolicy": "IfNotPresent",
-                            "command": ["sh", "-c", "export DRONE_ID=${HOSTNAME##*-}; exec python /opt/vkt/drone.py"],
+                            "command": ["/bin/sh", "-c", "export DRONE_ID=${HOSTNAME##*-}; exec python /opt/vkt/drone.py"],
                             "env": [
                                 {"name": "FLEET", "value": fleet},
                                 {"name": "POD_NAME", "valueFrom": {"fieldRef": {"fieldPath": "metadata.name"}}},
