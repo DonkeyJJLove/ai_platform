f7c1d50000fbc32acac3dad12f9ebaaeb228251a
Cha0s aka DonkeyJJLove or… <donkeyjjlove@protonmail.com>
2026-09-10 22:16:06 +0200

    test: rebind same-connection carrier scan expectation

--- cyber_lion/tests/test_p0_moon_same_connection_denial_carrier.py
+++ cyber_lion/tests/test_p0_moon_same_connection_denial_carrier.py
@@ -11,7 +11,7 @@ from tools.p0_moon_same_connection_denial_carrier import (
 from tools.p0_moon_same_connection_denial_contract import ATTACK_IDS,CREATE_TABLE_SURFACE,PRAGMA_SURFACE
 
 REPO="DonkeyJJLove/ai_platform"
-EXPECTED_SCAN="9c93b7bc51ae9329c86fa59fae486eb35281f5716c1c7c267e427fbc91764d1a"
+EXPECTED_SCAN="a23aaf4b6fe387da94163019f09dc80a05fb1a20b913c98fc67df43b879d337d"
 
 def inventory():
     root=Path(__file__).resolve().parents[2];sources={}
