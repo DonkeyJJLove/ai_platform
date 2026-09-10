738eb24675ef6b1d06edcd0fb658dce3725fb7e3
Cha0s aka DonkeyJJLove or… <donkeyjjlove@protonmail.com>
2026-09-10 22:15:46 +0200

    test: rebind MOON observer candidate to current production scan

--- tools/p0_moon_readonly_observer_falsification.py
+++ tools/p0_moon_readonly_observer_falsification.py
@@ -13,7 +13,7 @@ from tools.p0_moon_seven_binding import DIRECT_OBSERVER_BLOCKED,FENCE,PERMISSION
 from tools.p0_moon_attack_registry import live_attacks
 from tools.p0_readonly_sqlite_boundary import open_readonly_sqlite
 
-EXPECTED_SCAN_DIGEST="9c93b7bc51ae9329c86fa59fae486eb35281f5716c1c7c267e427fbc91764d1a"
+EXPECTED_SCAN_DIGEST="a23aaf4b6fe387da94163019f09dc80a05fb1a20b913c98fc67df43b879d337d"
 FENCE_PATH="/home/d2j3/.lion-moon-file-write-fence.sqlite3"
 TARGET_PATH="/home/d2j3/lion-p0-moon-replace-live-cert-r1.canary"
 TABLE="moon_file_write_effect"
