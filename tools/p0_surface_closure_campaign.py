3a7fc67bbc72c498476604fe1cd39614670a8681
Cha0s aka DonkeyJJLove or… <donkeyjjlove@protonmail.com>
2026-09-21 12:12:26 +0200

    test(r18): refreeze exact production scan currentness

--- tools/p0_surface_closure_campaign.py
+++ tools/p0_surface_closure_campaign.py
@@ -5,7 +5,7 @@ from typing import Mapping,Tuple
 from cyber_lion.contracts.complete_mediation import EffectSurfaceInventory
 from tools.p0_surface_closure_campaign_contract import ProviderFamilyClosurePlan,SurfaceClosureCampaign,SurfaceClosureWorkItem
 
-EXPECTED_SCAN_DIGEST="f2c3f7fbdce4b075d63ca94235a113cf54c694dce9c4e61c6b445d35cdcb70be"
+EXPECTED_SCAN_DIGEST="f941a40190907c2277ecbfb3f132b8b8f96979f14bfa388ba21752bd92b9aa1f"
 CERTIFIED_PARTIAL_SURFACE="8c6d0020a0816d674a783504d2a8ccc25e3e75c0d446057ba3f4450bd768f687"
 MOON_PROVIDERS=frozenset({
     ".github.workflows.moon-file-write.yml",
