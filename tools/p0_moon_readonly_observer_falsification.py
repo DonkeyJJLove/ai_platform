d2918dc54bd156d4126e2fd76c3a858eba8078e2
Cha0s aka DonkeyJJLove or… <donkeyjjlove@protonmail.com>
2026-09-24 20:25:46 +0200

    R24 sync stale-generation fencing: tools/p0_moon_readonly_observer_falsification.py

--- tools/p0_moon_readonly_observer_falsification.py
+++ tools/p0_moon_readonly_observer_falsification.py
@@ -13,7 +13,7 @@ from tools.p0_moon_seven_binding import DIRECT_OBSERVER_BLOCKED,FENCE,PERMISSION
 from tools.p0_moon_attack_registry import live_attacks
 from tools.p0_readonly_sqlite_boundary import open_readonly_sqlite
 
-EXPECTED_SCAN_DIGEST="503667910721668ccbeed78f00bbc11962df1eba9f99f87f1c9ea8fcc319e32d"
+EXPECTED_SCAN_DIGEST="bd629704beedeb43f0f218fc6909a2997668841730e63282424d336850945027"
 FENCE_PATH="/home/d2j3/.lion-moon-file-write-fence.sqlite3"
 TARGET_PATH="/home/d2j3/lion-p0-moon-replace-live-cert-r1.canary"
 TABLE="moon_file_write_effect"
