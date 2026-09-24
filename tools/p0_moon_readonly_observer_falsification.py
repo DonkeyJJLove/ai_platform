017771f88ef890cc5ddc648a8c4c71841fb2d895
Cha0s aka DonkeyJJLove or… <donkeyjjlove@protonmail.com>
2026-09-24 20:58:15 +0200

    R24 sync causal conversation final: tools/p0_moon_readonly_observer_falsification.py

--- tools/p0_moon_readonly_observer_falsification.py
+++ tools/p0_moon_readonly_observer_falsification.py
@@ -13,7 +13,7 @@ from tools.p0_moon_seven_binding import DIRECT_OBSERVER_BLOCKED,FENCE,PERMISSION
 from tools.p0_moon_attack_registry import live_attacks
 from tools.p0_readonly_sqlite_boundary import open_readonly_sqlite
 
-EXPECTED_SCAN_DIGEST="bd629704beedeb43f0f218fc6909a2997668841730e63282424d336850945027"
+EXPECTED_SCAN_DIGEST="c8ec3a589645b00e69cecb42ae77256db8ea4c1df23e45760a08c43b457aa942"
 FENCE_PATH="/home/d2j3/.lion-moon-file-write-fence.sqlite3"
 TARGET_PATH="/home/d2j3/lion-p0-moon-replace-live-cert-r1.canary"
 TABLE="moon_file_write_effect"
