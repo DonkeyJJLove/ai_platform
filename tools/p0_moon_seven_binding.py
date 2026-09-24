69c359ed786d5f728059e11c576c73f06ba9d290
Cha0s aka DonkeyJJLove or… <donkeyjjlove@protonmail.com>
2026-09-24 16:35:20 +0200

    R24 publish UX/OTP candidate: tools/p0_moon_seven_binding.py

--- tools/p0_moon_seven_binding.py
+++ tools/p0_moon_seven_binding.py
@@ -13,7 +13,7 @@ from tools.p0_moon_seven_binding_contract import (
     MoonSafeAttackSpec,MoonSevenBindingPlan,MoonSurfaceEvidenceBundle,
 )
 
-EXPECTED_SCAN_DIGEST="916f7af4b36798bce891de04db0a9797e91d6d0639162cb07d4cfed189c36f45"
+EXPECTED_SCAN_DIGEST="22c124f9a02053c67c61df1e2127c894831cebd58bd7bb74be735a05db3cd56a"
 SEVEN=tuple(sorted(RECEIPT_IMPLIED_MOON_SURFACES))
 PERMISSION="dbff98ee0801784d8616fc32d67dfbb2ea19fbfcc1cfbda829cf904953f5631b"
 FENCE=set(SEVEN)-{PERMISSION}
