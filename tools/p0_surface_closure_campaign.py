d8e20f241b8fa7e0a5ea424d0a4264645a0a3b65
Cha0s aka DonkeyJJLove or… <donkeyjjlove@protonmail.com>
2026-09-24 20:58:31 +0200

    R24 sync causal conversation final: tools/p0_surface_closure_campaign.py

--- tools/p0_surface_closure_campaign.py
+++ tools/p0_surface_closure_campaign.py
@@ -5,7 +5,7 @@ from typing import Mapping,Tuple
 from cyber_lion.contracts.complete_mediation import EffectSurfaceInventory
 from tools.p0_surface_closure_campaign_contract import ProviderFamilyClosurePlan,SurfaceClosureCampaign,SurfaceClosureWorkItem
 
-EXPECTED_SCAN_DIGEST="bd629704beedeb43f0f218fc6909a2997668841730e63282424d336850945027"
+EXPECTED_SCAN_DIGEST="c8ec3a589645b00e69cecb42ae77256db8ea4c1df23e45760a08c43b457aa942"
 CERTIFIED_PARTIAL_SURFACE="8c6d0020a0816d674a783504d2a8ccc25e3e75c0d446057ba3f4450bd768f687"
 MOON_PROVIDERS=frozenset({
     ".github.workflows.moon-file-write.yml",
