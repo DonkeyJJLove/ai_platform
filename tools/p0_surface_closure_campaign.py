9ecc224e60506234022526f4632cce2ac41f16ed
Cha0s aka DonkeyJJLove or… <donkeyjjlove@protonmail.com>
2026-09-24 20:26:11 +0200

    R24 sync stale-generation fencing: tools/p0_surface_closure_campaign.py

--- tools/p0_surface_closure_campaign.py
+++ tools/p0_surface_closure_campaign.py
@@ -5,7 +5,7 @@ from typing import Mapping,Tuple
 from cyber_lion.contracts.complete_mediation import EffectSurfaceInventory
 from tools.p0_surface_closure_campaign_contract import ProviderFamilyClosurePlan,SurfaceClosureCampaign,SurfaceClosureWorkItem
 
-EXPECTED_SCAN_DIGEST="503667910721668ccbeed78f00bbc11962df1eba9f99f87f1c9ea8fcc319e32d"
+EXPECTED_SCAN_DIGEST="bd629704beedeb43f0f218fc6909a2997668841730e63282424d336850945027"
 CERTIFIED_PARTIAL_SURFACE="8c6d0020a0816d674a783504d2a8ccc25e3e75c0d446057ba3f4450bd768f687"
 MOON_PROVIDERS=frozenset({
     ".github.workflows.moon-file-write.yml",
