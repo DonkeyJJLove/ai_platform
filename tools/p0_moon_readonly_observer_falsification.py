8f4acc3aa5701a8c6295bea72fe3c6dea039060f
Cha0s aka DonkeyJJLove or… <donkeyjjlove@protonmail.com>
2026-09-25 11:04:57 +0200

    R24 sync model routing layer: tools/p0_moon_readonly_observer_falsification.py

--- tools/p0_moon_readonly_observer_falsification.py
+++ tools/p0_moon_readonly_observer_falsification.py
@@ -13,7 +13,7 @@ from tools.p0_moon_seven_binding import DIRECT_OBSERVER_BLOCKED,FENCE,PERMISSION
 from tools.p0_moon_attack_registry import live_attacks
 from tools.p0_readonly_sqlite_boundary import open_readonly_sqlite
 
-EXPECTED_SCAN_DIGEST="4bbe50b172b3e5d367fd4adc6e33673dcf2a50f9b32adb321da87041791001a1"
+EXPECTED_SCAN_DIGEST="632d251adc44dc533282d74c6527c9e1862d18d55585356717de93acbf92083f"
 FENCE_PATH="/home/d2j3/.lion-moon-file-write-fence.sqlite3"
 TARGET_PATH="/home/d2j3/lion-p0-moon-replace-live-cert-r1.canary"
 TABLE="moon_file_write_effect"
