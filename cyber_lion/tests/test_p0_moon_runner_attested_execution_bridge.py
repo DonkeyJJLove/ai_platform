b17abaa34856fc83640d13a14e47af517f2e0526
Cha0s aka DonkeyJJLove or… <donkeyjjlove@protonmail.com>
2026-09-26 15:12:48 +0200

    R24: refresh test scan carrier

--- cyber_lion/tests/test_p0_moon_runner_attested_execution_bridge.py
+++ cyber_lion/tests/test_p0_moon_runner_attested_execution_bridge.py
@@ -27,7 +27,7 @@ from tools.p0_moon_runner_attested_bridge_contract import (
 import tools.p0_moon_runner_attested_execution_bridge as bridge
 
 REPOSITORY = "DonkeyJJLove/ai_platform"
-CURRENT_SCAN = "d5f2c172143484e616ec97f12e8f43fe498231c06f71e438c8b9a12cdc439964"
+CURRENT_SCAN = "d4c591fb9a4454a583e664559bf7580c29adf4f6488eee1ecb18593c957829cf"
 HISTORICAL_SCAN = "2e509f22b7684e465dbebba73886aa9eae74f166480cb7e46d5be90a02a566d3"
 LIVE_SOURCE_REVISION = "830f8c2e5561655dc35118c97f4574acc3bf0816"
 WORKFLOW_SOURCE = "tools/p0_moon_runner_attested_execution_bridge.workflow.source.yml"
